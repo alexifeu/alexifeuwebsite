1e16c5db61171fd2accb40423965cd77a3ea09b5
Alexander Rankl <Alexifeu@gmail.com>
2024-09-23 16:32:41 +0200

    MORE MOOORE

--- NEW/process_signup.php
+++ NEW/process_signup.php
@@ -2,6 +2,7 @@
 // Validate form input
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
+    $email = $_POST['email'];
     $password = $_POST['password'];
     $confirm_password = $_POST['confirm_password'];
 
