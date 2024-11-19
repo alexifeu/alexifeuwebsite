c498e0817ae64910064dcdfca22ed08faf908e15
AleRankl <alexifeu@gmail.com>
2024-11-19 13:13:30 +0100

    test

--- New/test.php
+++ New/test.php
@@ -26,6 +26,7 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="test.css" />
     <title>Document</title>
 </head>
 
